d602754556ea16954a1fdfbbecc14faa7d6a0cfd
Mauro Bellati <mauro.bellati@gmail.com>
2024-12-03 18:15:02 +0100

    Add 2024 02

---
+++ src/Y2024/Day02/Y2024Day02.cs
@@ -0,0 +1,38 @@
+namespace AdventOfCode.Y2024.Day02;
+
+using AdventOfCodeDotNet;
+using Report = List<int>;
+
+public class Y2024Day02 : Solver
+{
+    public object PartOne(List<string> input) => input.Select(ParseReport).Count(IsSafe);
+
+    public object PartTwo(List<string> input) => input.Select(ParseReport).Count(IsSafeWithDampener);
+
+    private static bool IsSafe(Report report)
+    {
+        /* A report only counts as safe if both of the following are true:
+           - The levels are either all increasing or all decreasing.
+           - Any two adjacent levels differ by at least one and at most three.
+        */
+        var diffs = report.Slide(2).Select(group => group[1] - group[0]).ToList();
+        return diffs.All(diff => Math.Abs(diff) is >= 1 and <= 3) &&
+               diffs.Select(Math.Sign).Distinct().Count() == 1;
+    }
+
+    private static bool IsSafeWithDampener(Report report) =>
+        // A report is safe even if it contains at most one level that makes it unsafe.
+        IsSafe(report) || ReportsWithoutOneElementEach(report).Any(IsSafe);
+
+    private static Report ParseReport(string line) => line.ExtractInts().ToList();
+
+    private static IEnumerable<Report> ReportsWithoutOneElementEach(Report report)
+    {
+        for (var i = 0; i < report.Count; i++)
+        {
+            var copy = new Report(report);
+            copy.RemoveAt(i);
+            yield return copy;
+        }
+    }
+}
