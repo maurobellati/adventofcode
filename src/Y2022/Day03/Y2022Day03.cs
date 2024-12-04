9a7d0933e642dea01a3ed11679d5f44f3e61006a
Mauro Bellati <mauro.bellati@gmail.com>
2024-12-04 22:31:58 +0100

    Add 2022 03

---
+++ src/Y2022/Day03/Y2022Day03.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode.Y2022.Day03;
+
+using AdventOfCodeDotNet;
+
+public class Y2022Day03 : Solver
+{
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public object PartOne(List<string> input) => input.Select(SplitInHalf).Select(UniqueRepetition).Select(Score).Sum();
+
+    public object PartTwo(List<string> input) => input.Chunk(3).Select(UniqueRepetition).Select(Score).Sum();
+
+    private static int Score(char @char) => Alphabet.IndexOf(@char, StringComparison.InvariantCulture) + 1;
+
+    private static string[] SplitInHalf(string line) => [line[..(line.Length / 2)], line[(line.Length / 2)..]];
+
+    private static char UniqueRepetition(IEnumerable<string> strings) =>
+        strings.Aggregate(
+                Alphabet.ToHashSet(),
+                (value, item) =>
+                {
+                    value.IntersectWith(item);
+                    return value;
+                })
+            .Single();
+}
