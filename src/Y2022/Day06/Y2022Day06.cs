6fef195052d881881ad05c96ceee2b8e4879fed6
Mauro Bellati <mauro.bellati@gmail.com>
2024-12-05 15:41:37 +0100

    Add 2022 06

---
+++ src/Y2022/Day06/Y2022Day06.cs
@@ -0,0 +1,17 @@
+namespace AdventOfCode.Y2022.Day06;
+
+using AdventOfCodeDotNet;
+
+public class Y2022Day06 : Solver
+{
+    public object PartOne(List<string> input) => Solve(input, 4);
+
+    public object PartTwo(List<string> input) => Solve(input, 14);
+
+    private static object Solve(List<string> input, int size) =>
+        input[0]
+            .Slide(size)
+            .Select((window, index) => (Window: window, Index: index))
+            .First(pair => pair.Window.Count == pair.Window.Distinct().Count())
+            .Index + size;
+}
